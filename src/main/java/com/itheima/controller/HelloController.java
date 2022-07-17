cb2c41736a18779dbb01182738def2c2e4de95dd
johnaonxing <99457761+johnaonxing@users.noreply.github.com>
2022-07-17 22:07:36 +0800

    pull commit

--- src/main/java/com/itheima/controller/HelloController.java
+++ src/main/java/com/itheima/controller/HelloController.java
@@ -16,7 +16,6 @@ public class HelloController {
     @GetMapping("")
     public String hello(){
         System.out.println("hello world....");
-        System.out.println("厉害");
         return "OK";
     }
 
