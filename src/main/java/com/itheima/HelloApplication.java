92fbd706c3eeeb4ee39736545df17a5ddb107605
xfx <xfx@atguigu.com>
2022-07-17 21:15:22 +0800

    first commit

---
+++ src/main/java/com/itheima/HelloApplication.java
@@ -0,0 +1,16 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author XFX
+ * @Date 2022/7/15 21:25
+ */
+
+@SpringBootApplication
+public class HelloApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class,args);
+    }
+}
---
+++ src/main/java/com/itheima/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.itheima.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author XFX
+ * @Date 2022/7/15 21:12
+ */
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("")
+    public String hello(){
+        System.out.println("hello world....");
+        return "OK";
+    }
+
+}
